39624548caf8325b9b6ee5bd05055f11e579f74a
brismuth <brismuth@gmail.com>
2018-02-05 00:56:00 -0700

    Adding autofocus on the first problem on each update

--- src/app/components/Speedtest.js
+++ src/app/components/Speedtest.js
@@ -1,19 +1,38 @@
-import { h } from 'preact' // eslint-disable-line no-unused-vars
+import { h, Component } from 'preact' // eslint-disable-line no-unused-vars
 import PreactRedux from 'preact-redux'
 import { getSpeedtest } from './../store/selectors/speedtest'
 import { submitProblemResponse } from './../store/actions/speedtest'
 import MathProblem from './MathProblem.js'
 const { connect } = PreactRedux
 
-const Speedtest = ({ speedtest, _submitProblemResponse }) => (
-  <div className='Speedtest page'>
-    <div className = 'math-problem-set'>
-      {speedtest.map((mathProblem, index) => (
-        !mathProblem.response ? <MathProblem mathProblem={mathProblem} problemIndex={index} key={index} autoFocus _submitProblemResponse={_submitProblemResponse} /> : null
-      ))}
-    </div>
-  </div>
-)
+class Speedtest extends Component {
+  constructor(props) {
+    super(props);
+    this.firstMathProblem;
+  }
+
+  componentWillReceiveProps() {
+    delete this.firstMathProblem;
+  }
+
+  render() {
+    return (<div className='Speedtest page'>
+      <div className = 'math-problem-set'>
+        {this.props.speedtest.map((mathProblem, index) => (
+          !mathProblem.response ? <MathProblem mathProblem={mathProblem} problemIndex={index} key={index} _submitProblemResponse={this.props._submitProblemResponse} ref={(problem) => { this.firstMathProblem = this.firstMathProblem || problem; }} /> : null
+        ))}
+      </div>
+    </div>)
+  }
+
+  componentDidMount() {
+    this.firstMathProblem.numInput.focus();
+  }
+
+  componentDidUpdate() {
+    this.firstMathProblem.numInput.focus();
+  }
+}
 
 export default connect(
   (state) => ({
