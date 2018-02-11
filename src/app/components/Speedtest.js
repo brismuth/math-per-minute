bdd98e036ceab968d94f575d04ececdaf18f0f47
brismuth <brismuth@gmail.com>
2018-02-10 17:36:22 -0700

    Auto scrolling as problems are solved

--- src/app/components/Speedtest.js
+++ src/app/components/Speedtest.js
@@ -8,13 +8,28 @@ const { connect } = PreactRedux
 class Speedtest extends Component {
   constructor(props) {
     super(props);
+    this.problemSet = null;
+    this.currentProblem = null;
+  }
+
+  updatePosition() {
+    const offset = this.currentProblem.base.offsetLeft - 160;
+    this.problemSet.style.left = `-${offset}px`;
+  }
+
+  componentDidMount() {
+    this.updatePosition();
+  }
+
+  componentDidUpdate() {
+    this.updatePosition();
   }
 
   render() {
     return (<div className='Speedtest page'>
-      <div className = 'math-problem-set'>
+      <div className='math-problem-set' ref={(problemSet) => { this.problemSet = problemSet; }}>
         {this.props.speedtest.map((mathProblem, index) => (
-          <MathProblem mathProblem={mathProblem} problemIndex={index} key={index} _submitProblemResponse={this.props._submitProblemResponse} />
+          <MathProblem mathProblem={mathProblem} problemIndex={index} key={index} _submitProblemResponse={this.props._submitProblemResponse} ref={(problem) => { if (mathProblem.isCurrentProblem) { this.currentProblem = problem; } }} />
         ))}
       </div>
     </div>)
@@ -23,7 +38,7 @@ class Speedtest extends Component {
 
 export default connect(
   (state) => ({
-    speedtest: getSpeedtest(state)
+    speedtest: getSpeedtest(state),
   }),
   (dispatch) => ({
     _submitProblemResponse: (problemIndex, value) => dispatch(submitProblemResponse(problemIndex, value))
