d1276922951ead568315cbe53b03460278cc0a39
brismuth <brismuth@gmail.com>
2018-02-10 19:02:31 -0700

    Speeding up module prep

--- prepModules.sh
+++ prepModules.sh
@@ -1,7 +1,9 @@
 # this replaces references to react and react-dom with references to preact-compat
 
 echo "Replacing module usage of 'react' with 'preact-compat'..."
-find ./node_modules -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/'"'"'react'"'"'/'"'"'preact-compat'"'"'/g'
+find ./node_modules/react* -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/'"'"'react'"'"'/'"'"'preact-compat'"'"'/g'
+find ./node_modules/react* -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/"react"/"preact-compat"/g'
 
 echo "Replacing module usage of 'react-dom' with 'preact-compat'..."
-find ./node_modules -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/'"'"'react-dom'"'"'/'"'"'preact-compat'"'"'/g'
+find ./node_modules/react* -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/'"'"'react-dom'"'"'/'"'"'preact-compat'"'"'/g'
+find ./node_modules/react* -type f -iname '*.js' -print0 | xargs -0 sed -i '' 's/"react-dom"/"preact-compat"/g'
