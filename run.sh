1757bfb967f07cbec671011783b44c7e7f7868db
brismuth <brismuth@gmail.com>
2018-02-12 01:42:06 -0700

    fixing access log path

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker rm -f math
 docker run -d \
 	--name math \
 	-p 3000:3000 \
-    -v /ssd_files/math/access.log:access.log \
+    -v /ssd_files/math/access.log:/usr/src/app/access.log \
     -v /ssd_files/math/logs/:/root/.pm2/logs/ \
     -e VIRTUAL_HOST=mathperminute.com \
     -e "LETSENCRYPT_HOST=mathperminute.com" \
