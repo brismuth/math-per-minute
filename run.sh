10673c79d244f0e3dd9703920e102005fb4a0287
brismuth <brismuth@gmail.com>
2018-02-12 01:41:15 -0700

    Adding mount for pm2 logs

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run -d \
 	--name math \
 	-p 3000:3000 \
     -v /ssd_files/math/access.log:access.log \
+    -v /ssd_files/math/logs/:/root/.pm2/logs/ \
     -e VIRTUAL_HOST=mathperminute.com \
     -e "LETSENCRYPT_HOST=mathperminute.com" \
     -e "LETSENCRYPT_EMAIL=brismuth@gmail.com" \
