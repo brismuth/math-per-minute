e28bc41dc6e373c81e9d9ea4a691c85cb30a5ae7
brismuth <brismuth@gmail.com>
2018-07-16 23:40:07 -0600

    Updating "About".

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -4,9 +4,15 @@ export default () => (
   <div className='About page'>
 	<div className='about-wrapper'>
 		<h2>About Math per Minute</h2>
-		<p>Call me crazy, but I think typing speed tests are fun.</p>
-		<p>I thought a speed test for math could be fun too, so I decided to make one.</p>
+		<p>I wanted to get some exposure to progressive web apps, and after losing the math speed round in Jackbox Party Pack 3's trivia game, I decided to build a math speed test of my own.</p>
 		<p>Read more about me on my <a href="https://brismuth.com/" target="_blank">blog</a>, or look me up on <a href="https://stackoverflow.com/users/1569320/brismuth" target="_blank">Stack Overflow</a>.</p>
+		<h2>Features</h2>
+		<ul>
+			<li>Counts the number of addition and subtraction problems you can solve in a minute</li>
+			<li>Progressive web app</li>
+			<li>Mobile friendly</li>
+			<li>Offline support</li>
+		</ul>
 		<p>If you want to see how many math problems you can do in one minute, <a href="/">click here</a>.</p>
 	</div>
   </div>
